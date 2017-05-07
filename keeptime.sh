349e795d643afb298f7aa39b3ededa7befccda54
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2017-05-06 21:02:16 -0700

    Initial commit. Setup code for timesheet.

---
+++ keeptime.sh
@@ -0,0 +1,11 @@
+DATE=$(date +%a-%m-%d-%Y)
+START_TIME=$(date +%X)
+echo "$START_TIME"
+# ssh schowdhuri@bdd3.tspankaj.com
+END_TIME=$(date +%X)
+echo "Worked from $START_TIME to $END_TIME" >> $DATE.time
+
+# Pushes time card update to github.
+git add -A
+git commit -m "Updated GitHub Time Card for $DATE"
+git push origin master
