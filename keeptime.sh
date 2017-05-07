928d931b5ced6c4aa397c337f093f0979e177d74
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2017-05-06 21:08:23 -0700

    Updated GitHub Time Card for Sat-05-06-2017

--- keeptime.sh
+++ keeptime.sh
@@ -1,7 +1,7 @@
 DATE=$(date +%a-%m-%d-%Y)
 START_TIME=$(date +%X)
 echo "$START_TIME"
-# ssh schowdhuri@bdd3.tspankaj.com
+ssh schowdhuri@bdd3.tspankaj.com
 END_TIME=$(date +%X)
 echo "Worked from $START_TIME to $END_TIME" >> $DATE.time
 
